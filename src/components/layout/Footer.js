aae72203d250ab0f373430e30203230ac424e881
Aqshola <aqsholafid2@gmail.com>
2021-05-26 11:37:29 +0700

    create layout for mobile

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,4 +1,4 @@
-import { Flex, Box, Heading, Text } from '@chakra-ui/react';
+import { Flex, Box, Heading, Text, Container } from '@chakra-ui/react';
 import Link from '../misc/Link';
 
 const textDecorFooter = [
@@ -14,61 +14,66 @@ const textDecorFooter = [
 
 export default function Footer(params) {
   return (
-    <Flex
-      as="footer"
-      minH="36"
-      backgroundColor="black"
-      color="white"
-      direction="column"
-      alignItems="center"
-      padding="2"
-      overflow="hidden"
-    >
-      <Box width="100%" display="flex" justifyContent="space-between" flex="1">
-        {textDecorFooter.map((text, i) => (
-          <Text
-            fontFamily="sans-serif"
-            textTransform="uppercase"
-            fontWeight={i % 2 === 0 ? 'bold' : 'light'}
-            whiteSpace="nowrap"
-            mr={['24', '24', '0']}
-            ml={['-16', '-12', '0']}
+    <Box as="footer" minH="36" backgroundColor="black" color="white">
+      <Container
+        maxW="container.xl"
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+      >
+        <Box
+          overflow="hidden"
+          width="100%"
+          display="flex"
+          justifyContent="space-between"
+          flex="1"
+        >
+          {textDecorFooter.map((text, i) => (
+            <Text
+              key={i}
+              fontFamily="sans-serif"
+              textTransform="uppercase"
+              fontWeight={i % 2 === 0 ? 'bold' : 'light'}
+              whiteSpace="nowrap"
+              mr={['24', '24', '0']}
+              ml={['-16', '-12', '0']}
+            >
+              {text}
+            </Text>
+          ))}
+        </Box>
+        <Box mt="10">
+          <Heading
+            as="h1"
+            fontWeight="light"
+            letterSpacing="widest"
+            textAlign="center"
+            fontSize="xl"
           >
-            {text}
+            Next Notion
+          </Heading>
+          <Text textAlign="center" w="96" mt="5" mb="5" fontSize="xs">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu
+            velit tempus erat egestas efficitur. In hac habitasse platea
+            dictumst. Fusce a nunc eget ligula suscipit finibus.
           </Text>
-        ))}
-      </Box>
-      <Box mt="10">
-        <Heading
-          as="h1"
-          fontWeight="light"
-          letterSpacing="widest"
-          textAlign="center"
-          fontSize="xl"
-        >
-          Next Notion
-        </Heading>
-        <Text textAlign="center" w="96" mt="5" mb="5" fontSize="xs">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu velit
-          tempus erat egestas efficitur. In hac habitasse platea dictumst. Fusce
-          a nunc eget ligula suscipit finibus.
-        </Text>
-        <Flex w="96" justifyContent="center" gridColumnGap="5">
-          <Link fontFamily="sans-serif">Twitter</Link>
-          <Link fontFamily="sans-serif">LinkedIn</Link>
-          <Link fontFamily="sans-serif">RSS</Link>
-        </Flex>
-      </Box>
-      <Box>
-        <Text
-          textAlign="center"
-          mt="10"
-          fontSize="12px"
-          fontFamily="sans-serif"
-        >
-          © 2012–2020 Nordic Rose Co. <br></br> All rights reserved.{' '}
-        </Text>
-      </Box>
-    </Flex>
+          <Flex w="96" justifyContent="center" gridColumnGap="5">
+            <Link fontFamily="sans-serif">Twitter</Link>
+            <Link fontFamily="sans-serif">LinkedIn</Link>
+            <Link fontFamily="sans-serif">RSS</Link>
+          </Flex>
+        </Box>
+        <Box>
+          <Text
+            textAlign="center"
+            mt="10"
+            fontSize="xs"
+            fontFamily="sans-serif"
+          >
+            © 2021 Next Notion <br></br> All rights reserved.{' '}
+          </Text>
+        </Box>
+      </Container>
+    </Box>
   );
 }
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,31 +1,77 @@
-import { Flex, Box, Heading } from '@chakra-ui/react';
+import { Flex, Box, IconButton, Heading, Container } from '@chakra-ui/react';
 import Link from '../misc/Link';
 export default function Header(params) {
   return (
-    <Flex
-      padding="5"
-      alignItems="center"
-      as="header"
-      borderBottom="1px"
-      borderColor="gray.200"
-    >
-      <Heading as="h1" fontWeight="light" letterSpacing="widest" flexGrow="1">
-        Next Notion
-      </Heading>
-      <Flex w="72" justifyContent="space-between">
-        <Link fontWeight="bold" fontFamily="serif">
-          BLOG
-        </Link>
-        <Link fontWeight="bold" fontFamily="serif">
-          ABOUT
-        </Link>
-        <Link fontWeight="bold" fontFamily="serif">
-          LINKS
-        </Link>
-        <Link fontWeight="bold" fontFamily="serif">
-          PROJECTS
-        </Link>
-      </Flex>
-    </Flex>
+    <Box as="header" borderBottom="1px" borderColor="gray.200" py="4">
+      <Container maxW="container.xl">
+        <Flex alignItems="center">
+          <Heading
+            as="h1"
+            fontWeight="light"
+            letterSpacing="widest"
+            flexGrow="1"
+            fontSize={['2xl', '2xl', '3xl']}
+          >
+            Next Notion
+          </Heading>
+          <Flex
+            w="72"
+            justifyContent="space-between"
+            display={['none', 'none', 'flex']}
+          >
+            <Link fontWeight="bold" fontFamily="serif">
+              BLOG
+            </Link>
+            <Link fontWeight="bold" fontFamily="serif">
+              ABOUT
+            </Link>
+            <Link fontWeight="bold" fontFamily="serif">
+              LINKS
+            </Link>
+            <Link fontWeight="bold" fontFamily="serif">
+              PROJECTS
+            </Link>
+          </Flex>
+          <IconButton
+            display={['flex', 'flex', 'none']}
+            size="lg"
+            bgColor="white"
+            icon={
+              <svg
+                width="24px"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            }
+          />
+        </Flex>
+        <Flex
+          py="0"
+          w="72"
+          mx="auto"
+          justifyContent="space-between"
+          display="flex"
+          fontSize="sm"
+          fontWeight="bold"
+          fontFamily="serif"
+          visibility="hidden"
+          h="0"
+        >
+          <Link>BLOG</Link>
+          <Link>ABOUT</Link>
+          <Link>LINKS</Link>
+          <Link>PROJECTS</Link>
+        </Flex>
+      </Container>
+    </Box>
   );
 }
