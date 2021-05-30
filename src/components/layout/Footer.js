618414288570716497d73b8643b61a4094779e42
Aqshola <aqsholafid2@gmail.com>
2021-05-30 19:19:33 +0700

    fix margin-top footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -14,7 +14,7 @@ const textDecorFooter = [
 
 export default function Footer(params) {
   return (
-    <Box as="footer" backgroundColor="black" color="white">
+    <Box as="footer" backgroundColor="black" color="white" mt="5">
       <Container
         maxW="container.xl"
         display="flex"
