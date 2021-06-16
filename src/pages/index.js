6fdec76fbdf6aa8fa36dfd4dc0ce4594036456c7
Naufaldi <naufaldi.rafif@gmail.com>
2021-06-16 17:45:48 +0700

    add list blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,31 @@
 import Layout from '../components/layout/Layout';
-import { Container, Text } from '@chakra-ui/react';
+import { Container } from '@chakra-ui/react';
 import HeroHome from '@/components/Home/HeroHome';
+import { getDatabase } from '@/lib/notion';
 import ListArticle from '@/components/Home/ListArticle';
 
-const Index = () => (
-  <Layout>
-    <Container maxW="container.xl">
-      <HeroHome />
-      <ListArticle />
-    </Container>
-  </Layout>
-);
+export const databaseId = process.env.NOTION_DATABASE_ID;
+
+const Index = ({ posts }) => {
+  console.log('Post ', posts);
+  return (
+    <Layout>
+      <Container maxW="container.xl">
+        <HeroHome />
+        <ListArticle posts={posts} />
+      </Container>
+    </Layout>
+  );
+};
+
+export const getStaticProps = async () => {
+  const database = await getDatabase(databaseId);
+  return {
+    props: {
+      posts: database,
+    },
+    revalidate: 1,
+  };
+};
 
 export default Index;
