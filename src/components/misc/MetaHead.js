847aac3f13524051155865a41324eeca6e00c24c
Aqshola <aqsholafid2@gmail.com>
2021-06-18 21:02:09 +0700

    fix metahead and connect with twitter card and fb og

--- src/components/misc/MetaHead.js
+++ src/components/misc/MetaHead.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import { useEffect, useState } from 'react';
-export default function MetaHead({ title, description, heroImage }) {
+export default function MetaHead({ title, description, heroImage, type }) {
   const [canonLink, setcanonLink] = useState('');
   const [hostLink, sethostLink] = useState('');
 
@@ -17,11 +17,12 @@ export default function MetaHead({ title, description, heroImage }) {
       <meta name="description" content={description} />
       <link rel="canonical" href={canonLink} />
       <meta property="og:url" content={canonLink} />
-      <meta property="og:type" content="article" />
+      <meta property="og:type" content={type} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={heroImage} />
-      <meta name="twitter:card" content={description} />
+      <meta property="og:image" content={process.env.DOMAIN + heroImage} />
+      <meta name="twitter:card" content="summary" />
+      <meta property="fb:app_id" content="793139241341140" />
       <meta name="twitter:site" content={hostLink} />
     </Head>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,24 @@
 import Layout from '../components/layout/Layout';
-import { Container, Text } from '@chakra-ui/react';
+import { Container } from '@chakra-ui/react';
 import HeroHome from '@/components/Home/HeroHome';
 import ListArticle from '@/components/Home/ListArticle';
+import MetaHead from '@/components/misc/MetaHead';
 
 const Index = () => (
-  <Layout>
-    <Container maxW="container.xl">
-      <HeroHome />
-      <ListArticle />
-    </Container>
-  </Layout>
+  <>
+    <MetaHead
+      title="Next Notion Homepage"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, optio molestiae soluta eaque eius molestias repudiandae quia consectetur at possimus, sunt itaque tenetur fugit culpa, tempore magni reiciendis omnis explicabo?"
+      heroImage="/img/hero-img.png"
+      type={'website'}
+    />
+    <Layout>
+      <Container maxW="container.xl">
+        <HeroHome />
+        <ListArticle />
+      </Container>
+    </Layout>
+  </>
 );
 
 export default Index;
